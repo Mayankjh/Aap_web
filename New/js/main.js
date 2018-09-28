a8ddf22aad79dd3997fd1839e54ee55b9cefae18
Mayank Jha <mayankjha7722@gmail.com>
2018-09-29 01:16:28 +0530

    Map added!!

--- New/js/main.js
+++ New/js/main.js
@@ -284,7 +284,7 @@ $(document).ready(function() {
         function init() {
             var mapOptions = {
                 zoom: 11,
-                center: new google.maps.LatLng(40.6700, -73.9400), // New York
+                center: new google.maps.LatLng(28.5444, 77.23255), // New York
                 styles: [{
                     "featureType": "water",
                     "elementType": "geometry",
